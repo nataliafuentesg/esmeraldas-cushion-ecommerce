06bb61cf8f18fbae428da959051e54bfdbaf39ea
nataliafuentes <nataliaf1412@gmail.com>
2026-05-11 16:27:45 -0500

    og-cushion

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import { useAuthStore } from '@/stores/auth'
-import Catalog from '@/views/Catalog.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
